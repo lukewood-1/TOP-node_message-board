85133853b953531a48bfa3cd77740bf69c33ba03
Lucas Lopes <173523829+lukewood-1@users.noreply.github.com>
2025-07-13 21:02:32 -0300

    fix: Change db connectionString to host platform's format

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const db = new Pool({
-  connectionString: `postgres://${process.env.NAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.PATH}`
+  connectionString: process.env.connectionString
 });
 
 export default db
